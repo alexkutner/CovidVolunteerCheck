2aa5e5b542e8fbdbd8a267967c6db59edab2c0fb
alexkutner <kutner@gmail.com>
2021-02-01 11:34:01 -0800

    a bit of cleanup and adding latest time

--- lambda_function.py
+++ lambda_function.py
@@ -1,20 +1,18 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+RESERVATION_URL = "https://volunteer.covidvaccineseattle.org/RegMobile.aspx"
 
 DEFAULT_VALUE = {'SELECT ONE',
                  'SeattleU Jan 25th through 30th: Washington',
                  'SeattleU Jan 18th through 23rd: Washington',
                  'SeattleU Feb 1st through 6th: Washington',
-                 'SeattleU Feb 8th through 13th: Washington'}
+                 'SeattleU Feb 8th through 13th: Washington',
+                 'SeattleU Feb 15th through 20th: Washington'}
 
 import json
 import urllib.request
 from bs4 import BeautifulSoup
 
 def lambda_handler(event, context):
-    contents = urllib.request.urlopen("https://volunteer.covidvaccineseattle.org/RegMobile.aspx").read()
+    contents = urllib.request.urlopen(RESERVATION_URL).read()
     soup = BeautifulSoup(contents, 'html.parser')
 
     dropdownvalues = soup.find(id='ContentPlaceHolder1_DropDownListOpKey').find_all('option')
@@ -23,10 +21,8 @@ def lambda_handler(event, context):
         if value.get_text() not in DEFAULT_VALUE:
             return {
                 'statusCode': 200,
-                'body': json.dumps('Hello from Lambda!')
+                'body': json.dumps('New times found')
             }
-            print('we found one!')
-
     return {
         'statusCode': 404,
         'body': json.dumps('no new resource')
